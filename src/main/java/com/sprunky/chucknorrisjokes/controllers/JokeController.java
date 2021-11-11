bc69cadc0286519666bafa63dec5988d81838b4d
jonathan.garrido <jonathan.garrido@incentro.com>
2021-11-11 12:18:39 +0100

    removed requestmapping since default is enough.

--- src/main/java/com/sprunky/chucknorrisjokes/controllers/JokeController.java
+++ src/main/java/com/sprunky/chucknorrisjokes/controllers/JokeController.java
@@ -4,10 +4,8 @@ import com.sprunky.chucknorrisjokes.service.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller()
-@RequestMapping("/")
 public class JokeController {
 
     private final JokeService jokeService;
